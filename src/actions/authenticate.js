aac74b915ee8d7f78bcb0c1030762536dbd82da2
tomrw <tomrichardwest@gmail.com>
2016-08-14 11:58:50 +0100

    Unwrap promise into deferred

--- src/actions/authenticate.js
+++ src/actions/authenticate.js
@@ -1,24 +1,27 @@
+import { defer } from 'es6-promise-extended';
 import connection from '../connection/db';
 
 export default function authenticate(username, password) {
-	return new Promise((resolve, reject) => {
-		connection('users')
-			.where({
-				username,
-				password
-			})
-			.count('* AS count')
-			.then((result) => {
-				const isAuthenticated = result[0].count === 1;
+	const deferred = defer();
 
-				if (isAuthenticated) {
-					resolve();
-				} else {
-					reject();
-				}
-			})
-			.catch(() => {
-				reject();
-			});
-	});
+	connection('users')
+		.where({
+			username,
+			password
+		})
+		.count('* AS count')
+		.then((result) => {
+			const isAuthenticated = result[0].count === 1;
+
+			if (isAuthenticated) {
+				deferred.resolve();
+			} else {
+				deferred.reject();
+			}
+		})
+		.catch(() => {
+			deferred.reject();
+		});
+
+	return deferred.promise;
 }
