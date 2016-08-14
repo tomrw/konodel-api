16ec8ae6f918a8e8d7296a49f7f51ae5865b8afe
tomrw <tomrichardwest@gmail.com>
2016-08-14 09:32:17 +0100

    Optimise the login SL query

--- src/actions/authenticate.js
+++ src/actions/authenticate.js
@@ -3,11 +3,11 @@ import connection from '../connection/db';
 export default function authenticate(username, password) {
 	return new Promise((resolve, reject) => {
 		connection.connect(() => {
-			const query = 'SELECT * FROM users WHERE username = ? AND password = ? LIMIT 1';
+			const query = 'SELECT COUNT(*) AS count FROM users WHERE username = ? AND password = ? LIMIT 1';
 			const params = [ username, password ];
 
 			connection.query(query, params, function (err, result) {
-				const isAuthenticated = result.length === 1;
+				const isAuthenticated = result[0].count === 1;
 
 				if (isAuthenticated) {
 					resolve();
